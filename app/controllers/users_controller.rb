a20888e8251dfed7cea34f672d283df7a272ea9f
Zach Dicklin <zdicklin@zdicklin-laptop.(none)>
2010-11-17 22:46:22 -0600

    Heroku Test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -127,7 +127,7 @@ class UsersController < ApplicationController
     # Chart Listing
     # Find number of charts associated with this user
     @charts = Chart.where( :creator => session[:user_id] )
-    #@chart_count = @charts.size
+    @chart_count = @charts.size
   end
 
 end
