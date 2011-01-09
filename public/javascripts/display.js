38c2d9f98f8eb3746035764e0aa74fe2cbd4295e
Zach Dicklin <zdicklin@zdicklin-laptop.(none)>
2011-01-09 17:06:34 -0600

    Adjust screen width

---
+++ public/javascripts/display.js
@@ -0,0 +1,22 @@
+/**
+ * Display Controls
+ */
+
+/**
+ * Get Browser Window Width
+ * 
+ * @return {int} Window Width
+ */
+function getWindowWidth()
+{
+	return window.innerWidth;
+}
+
+/**
+ * Get Browser Window Height
+ * @return {int} Window Height
+ */
+function getWindowHeight()
+{
+	return window.innerHeight;
+}
